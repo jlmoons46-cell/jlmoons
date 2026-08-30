605ad268fed15a963df029d60fb4f820ef3fc92b
JLMOONS <jlmoons46@gmail.com>
2026-08-30 21:01:08 +0000

    Install Google Tag Manager Copy the code below and paste it on to every

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MPM9RQPW');`,
+            })(window,document,'script','dataLayer','GTM-KW2RN838');`,
           }}
         />
         {/* End Google Tag Manager */}
@@ -34,7 +34,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MPM9RQPW"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KW2RN838"
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
